92fc373e4c93a385e6c422ad8b7b56ff194660e2
cparragatorres <humbertpt14@gmail.com>
2022-06-07 18:45:29 -0500

    Home 6

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -8,8 +8,31 @@ class ChatPage extends StatelessWidget {
     return Scaffold(
       body: ListView.builder(
         itemCount: 10,
-        itemBuilder: (BuildContext context, int index){
-          return Text("item");
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.black12,
+              radius: 24,
+              backgroundImage: NetworkImage(
+                "https://acortar.link/pwag9x",
+              ),
+            ),
+            title: Text(
+              "Fatima de las Nieves",
+              style: TextStyle(
+                fontSize: 14.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            subtitle: Text(
+              "He enviado los archivos que solicitaste, por favor los revisas",
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                fontSize: 13.0,
+              ),
+            ),
+          );
         },
       ),
     );
