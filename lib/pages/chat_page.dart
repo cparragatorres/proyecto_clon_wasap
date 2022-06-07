3437f9854b7de408cc3ec09dfacab00f59ba7824
cparragatorres <humbertpt14@gmail.com>
2022-06-07 18:56:15 -0500

    Home 8

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -9,54 +9,61 @@ class ChatPage extends StatelessWidget {
       body: ListView.builder(
         itemCount: 10,
         itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Colors.black12,
-              radius: 24,
-              backgroundImage: NetworkImage(
-                "https://acortar.link/pwag9x",
-              ),
-            ),
-            title: Text(
-              "Fatima de las Nieves",
-              style: TextStyle(
-                fontSize: 14.0,
-                fontWeight: FontWeight.w600,
-              ),
-            ),
-            subtitle: Text(
-              "He enviado los archivos que solicitaste, por favor los revisas",
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(
-                fontSize: 13.0,
-              ),
-            ),
-            trailing: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  "20:24",
-                  style: TextStyle(
-                    fontSize: 12.0,
-                    // color: Colors.black.withOpacity(0.45),
-                    color: Color(0xff00A884),
-                  ),
+          return Container(
+            margin: const EdgeInsets.only(bottom: 8.0),
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.black12,
+                radius: 24,
+                backgroundImage: NetworkImage(
+                  "https://acortar.link/pwag9x",
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Color(0xff00A884),
-                  ),
-                  child: Text(
-                    "9",
+              ),
+              title: Text(
+                "Fatima de las Nieves",
+                style: TextStyle(
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              subtitle: Text(
+                "He enviado los archivos que solicitaste, por favor los revisas",
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontSize: 13.0,
+                ),
+              ),
+              trailing: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    "20:24",
                     style: TextStyle(
-                      color: Colors.white,
                       fontSize: 12.0,
+                      // color: Colors.black.withOpacity(0.45),
+                      color: Color(0xff00A884),
                     ),
                   ),
-                ),
-              ],
+                  Container(
+                    height: 20.0,
+                    width: 20.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(0xff00A884),
+                    ),
+                    alignment: Alignment.center,
+                    child: Text(
+                      "9",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           );
         },
