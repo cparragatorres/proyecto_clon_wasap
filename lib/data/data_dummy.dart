54959b900d873d103f3c5b2c2a37eab0cd0276c2
cparragatorres <humbertpt14@gmail.com>
2022-06-07 19:09:49 -0500

    Home 9.1

---
+++ lib/data/data_dummy.dart
@@ -0,0 +1,20 @@
+import 'package:proyecto_clon_wasap/models/chat_model.dart';
+
+List<ChatModel> chatList = [
+  ChatModel(
+    avatarURL: "",
+    username: "Ximena Lopez",
+    message: "Ya he enviado el encargo por el courier",
+    time: "10:20",
+    countMessage: 1,
+    isTyping: false,
+  ),
+  ChatModel(
+    avatarURL: "",
+    username: "Ximena Lopez",
+    message: "Ya he enviado el encargo por el courier",
+    time: "10:20",
+    countMessage: 1,
+    isTyping: false,
+  ),
+];
